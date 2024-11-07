10880b7b344be1af2a6fad4fcd1332d2b9cfe71b
karim <karimloco4@gmail.com>
2024-11-07 16:39:07 +0100

    legacy code refacoring

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,32 +1,36 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.IUserSession;
 import org.craftedsw.tripservicekata.user.User;
-import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
+    private final IUserSession userSession;
+    private final ITripDAO tripDAO;
+
+
+    public TripService(IUserSession userSession, ITripDAO tripDAO) {
+        this.userSession = userSession;
+        this.tripDAO = tripDAO;
+    }
+
+
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<Trip>();
-        User loggedUser = UserSession.getInstance().getLoggedUser();
-        boolean isFriend = false;
-        if (loggedUser != null) {
-            for (User friend : user.getFriends()) {
-                if (friend.equals(loggedUser)) {
-                    isFriend = true;
-                    break;
-                }
-            }
-            if (isFriend) {
-                tripList = TripDAO.findTripsByUser(user);
-            }
-            return tripList;
+        if (this.userSession.isLoggedIn()) {
+            return hasFriends(user) ? this.tripDAO.findTripsByUser(user) : Collections.emptyList();
         } else {
             throw new UserNotLoggedInException();
         }
+    }
+
+    public boolean hasFriends(User user) {
+        return user.getFriends()
+                .stream()
+                .anyMatch(friend -> friend.equals(this.userSession.getLoggedUser()));
     }
 
 }
--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,14 +1,65 @@
 package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.IUserSession;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
 
-public class TripServiceTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TripServiceTest {
+
+    TripService tripService;
+    @Mock
+    IUserSession userSession;
+    @Mock
+    ITripDAO tripDAO;
+
+    @BeforeEach
+    void setUp() {
+        tripService = new TripService(userSession, tripDAO);
+    }
+
+    @Test
+    void should_throw_user_not_logged_in() {
+        assertThrows(UserNotLoggedInException.class,()-> tripService.getTripsByUser(null));
+    }
+
+    @Test
+    void should_return_empty_list_when_user_logged_in_and_has_no_friends() {
+        when(this.userSession.isLoggedIn()).thenReturn(true);
+        assertEquals(Collections.emptyList(), tripService.getTripsByUser(new User()));
+    }
+
+    @Test
+    void should_return_list_when_user_logged_in_and_has_friends() throws UserNotLoggedInException {
+        User friend1 = new User();
+        User loggedUser = new User();
+        User user = new User();
+        user.addFriend(friend1);
+        user.addFriend(loggedUser);
+        //
+        Trip trip = new Trip();
+        user.addTrip(trip);
+        List<Trip> trips = Collections.singletonList(trip);
+        //
+        when(this.userSession.isLoggedIn()).thenReturn(true);
+        when(this.userSession.getLoggedUser()).thenReturn(loggedUser);
+        when(this.tripDAO.findTripsByUser(user)).thenReturn(trips);
+        assertEquals(trips, tripService.getTripsByUser(user));
+    }
 
 }
